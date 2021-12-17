4d787f3e60ff6f97db7a8744e118c5caecb3e5cc
CuzImBisonratte <CuzImBisonratte@gmail.com>
2021-12-17 21:03:01 +0100

    added commentary

--- Libraries/formatter.js
+++ Libraries/formatter.js
@@ -1,7 +1,14 @@
+// Function to format a number with dots as thousands separator
 function formatNumber(number) {
+
+    // Replace the decimal point
     var numberNew = number.toString().split(".")[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+    // replace english point with german comma
     if (number.toString().split(".")[1] != undefined) {
         numberNew = numberNew + "," + number.toString().split(".")[1];
     }
+
+    // return the formatted number
     return numberNew;
 }
